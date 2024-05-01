45b08c279ae3cc0cb3da2ab57c25c9391dcf83fc
Lucas Gladding <lucasgladding@users.noreply.github.com>
2024-05-01 12:55:59 -0400

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Lucas Gladding on 2024-05-01.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
